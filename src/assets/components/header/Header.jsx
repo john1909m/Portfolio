d5477b684944b7f5d13641e7fdedefcaf960b640
john1909m <johnemil21@yahoo.com>
2025-04-10 19:52:50 +0200

    first

---
+++ src/assets/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.scss'
+const Header = () => {
+  return (
+    <>
+        <div className='cover-container'>
+            <img className='' src="/src/assets/images/create_a_photo_that_manipulates_graphic_design_programs_with_vs_code_programming_on_a_laptop_from_a_zuftximqnskfphaymy63_1.webp" alt="Cover photo" />
+        
+        </div>
+    </>
+  )
+}
+
+export default Header
